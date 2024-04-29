3daf8778c162cc7dbacfa7ca6cea5cc051b72e0f
Kidist Berhanu <145928754+kidist-berhanu@users.noreply.github.com>
2024-04-29 20:32:54 +0300

    Update script.js

--- Signup Page/script.js
+++ Signup Page/script.js
@@ -1,1 +1,8 @@
+document.getElementById("signupForm").addEventListener("submit", function(event){
+    event.preventDefault();
+    
+    const formData = new FormData(event.target);
+    const formDataObject = Object.fromEntries(formData.entries());
+    console.log(formDataObject);
+});
 
