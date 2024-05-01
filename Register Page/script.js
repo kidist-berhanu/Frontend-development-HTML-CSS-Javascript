456e09b398a7d616e00bfcff9ba5ef435c499727
Kidist Berhanu <145928754+Kindatryouts@users.noreply.github.com>
2024-05-01 21:54:34 +0300

    adding style

---
+++ Register Page/script.js
@@ -0,0 +1,12 @@
+document.getElementById("registrationForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    var fullname = document.getElementById("fullname").value;
+    var email = document.getElementById("email").value;
+    var phone = document.getElementById("phone").value;
+    var dob = document.getElementById("dob").value;
+    var gender = document.getElementById("gender").value;
+    var education = document.getElementById("education").value;
+    
+    //alert(fullname + " registered successfully!");
+  });
+  
