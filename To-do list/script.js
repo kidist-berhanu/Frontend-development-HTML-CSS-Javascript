428801969b37329ad6e4aeca510b338aa7195f7c
Kidist Berhanu <145928754+kidist-berhanu@users.noreply.github.com>
2024-06-06 21:26:00 +0300

    Update script.js

--- To-do list/script.js
+++ To-do list/script.js
@@ -6,21 +6,3 @@ function addTask() {
         alert('Please enter a task.');
         return;
     }
-
-    const taskList = document.getElementById('taskList');
-    const li = document.createElement('li');
-
-    li.textContent = taskValue;
-
-    const span = document.createElement('span');
-    span.textContent = '×';
-    span.className = 'close';
-    span.onclick = function () {
-        taskList.removeChild(li);
-    };
-
-    li.appendChild(span);
-    taskList.appendChild(li);
-    taskInput.value = '';
-}
-
